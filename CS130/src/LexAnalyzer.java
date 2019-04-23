07e51fee035811e0e9c6bb26eb355f48bb7cd127
MikolovDeang <49778813+MikolovDeang@users.noreply.github.com>
2019-04-23 19:20:18 +0800

    Add files via upload

---
+++ CS130/src/LexAnalyzer.java
@@ -0,0 +1,180 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class LexAnalyzer {
+	static ArrayList<Token> tokens = new ArrayList<Token>();
+	static String input;
+	public static void main(String[] args) {
+	    try { 
+	        String s = "";
+	        @SuppressWarnings("resource")
+			BufferedReader in = new BufferedReader(new FileReader(new File("D:\\Java Projects\\CS130\\Sample.txt")));
+	        while((s = in.readLine()) != null) {
+	            input += s;
+	        }
+	        input = input.trim();
+	        while(input.length() != 0)
+	        {
+	        	Token t = getNewToken();
+	        	if(t != null && t.getLexeme() != null && t.getLexeme().trim().length() != 0)
+	        		tokens.add(t);
+	        }
+	        System.out.println("TOKEN       LEXEME");
+	        System.out.println("------------------");
+	        for(int i = 0; i < tokens.size(); i++) {
+	            System.out.println(tokens.get(i).getIDLexeme());
+	        }
+	    }
+	    catch(Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+	public static Token getNewToken()
+	{
+		if(input.charAt(0) == '<')
+		{
+			if(input.charAt(1) == '/')
+			{
+				input = input.substring(2, input.length());
+				return new Token("ENDTAGHEAD","</");
+			}
+			else if(input.substring(0, 4).equals("<!--"))
+			{
+				int i = 4;
+				while(!input.substring(i, i + 3).equals("-->"))
+				{
+					i++;
+				}
+				input = input.substring(i + 3, input.length());
+				return null;
+			}
+			else if(isNumeric(input.substring(1,3).trim()) || isNumeric(input.substring(1,2).trim()))
+			{
+				input = input.substring(1, input.length());
+				return new Token("LTHAN","<");
+			}
+			
+			String lexeme = "";
+			int i = 0;
+			while(input.charAt(i) != '>')
+			{
+				lexeme += input.charAt(i++);
+			}
+			input = input.substring(i, input.length());
+			return new Token("TAGIDENT",lexeme);
+		}
+		else if(input.charAt(0) == '>')
+		{
+			input = input.substring(1, input.length());
+			return new Token("GTHAN",">");
+		}
+		else
+		{
+			Token temp = checkToken(0);
+			if(temp != null)
+			{
+				input = input.substring(1, input.length());
+				return temp;
+			}
+			else if(input.subSequence(0, 3).equals("**"))
+			{
+				input = input.substring(2, input.length());
+				return new Token("EXP","**");
+			}
+			String lexeme = "";
+			int i = 0;
+			while(input.charAt(i) != '<' && input.charAt(i) != '>' && checkToken(i) == null)
+			{
+				lexeme += input.charAt(i++);
+				if(i == input.length())
+					break;
+			}
+			input = input.substring(i, input.length());
+			try {
+				Double.parseDouble(lexeme);
+	            return new Token("NUMBER",lexeme);
+	        } catch (NumberFormatException e) {
+	        	if(lexeme.equals(null) || lexeme.equals("null"))
+	        	{
+	        		return null;
+	        	}
+	        	else
+	        	{
+	        		if(lexeme.equals("."))
+	        		{
+	        			return new Token("PERIOD",".");
+	        		}
+	        		else
+	        		{
+	        			return new Token("IDENT",lexeme);
+	        		}
+	        	}
+	        }
+		}
+	}
+	public static Token checkToken(int i)
+	{
+		char temp = input.charAt(i);
+		Token t = null;
+		switch(temp)
+		{
+			case '+':
+				t = new Token("PLUS","+");
+				break;
+			case '-':
+				t = new Token("MINUS","-");
+				break;
+			case '*':
+				t = new Token("MULT","*");
+				break;
+			case '/':
+				t = new Token("DIVIDE","/");
+				break;
+			case '%':
+				t = new Token("MODULO","%");
+				break;
+			case '[':
+				t = new Token("LBRACKET","[");
+				break;
+			case ']':
+				t = new Token("RBRACKET","]");
+				break;
+			case '(':
+				t = new Token("LPAREN","(");
+				break;
+			case ')':
+				t = new Token("RPAREN",")");
+				break;
+			case ':':
+				t = new Token("COLON",":");
+				break;
+			case ',':
+				t = new Token("COMMA",",");
+				break;
+			case ';':
+				t = new Token("SCOLON",";");
+				break;
+			case '=':
+				t = new Token("EQUALS","=");
+				break;
+			case '"':
+				t = new Token("DQUOTE","\"");
+				break;
+			case '\'':
+				t = new Token("QUOTE","\'");
+				break;
+		}
+		return t;
+	}	
+	public static boolean isNumeric(String in)
+	{
+		try {
+			Double.parseDouble(in);
+			return true;
+        } catch (NumberFormatException e) {
+        	return false;
+        }
+	}
+}
---
+++ CS130/src/Token.java
@@ -0,0 +1,26 @@
+public class Token {
+    String id, lexeme;
+
+    public Token(String id, String lexeme) {
+        this.id = id;
+        this.lexeme = lexeme.trim();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+    
+    public String getIDLexeme()
+    {
+    	String whitespace = "";
+    	for(int i = 0; i < 12 - id.length(); i++)
+    	{
+    		whitespace += " ";
+    	}
+    	return id + whitespace + lexeme;
+    }
+}
